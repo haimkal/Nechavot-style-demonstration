ce624430742157c87ecddb54e4c816d541d81aca
Haim Kalvo <you@example.com>
2021-09-30 15:22:38 +0300

    adding instructions

--- src/NechavotDemo/NechavotDemo.js
+++ src/NechavotDemo/NechavotDemo.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useState } from 'react';
 import Card from './Card/Card';
 import './NechavotDemo.scss';
 
 export default function NechavotDemo() {
+
+    const [showInstructions, setShowInstructions] = useState(true);
+
     const images = [
         { title: "Login", source: 'https://i.ibb.co/PZVj75q/Login-1.jpg' },
         { title: "Feed", source: 'https://i.ibb.co/2S7Yn3h/Feed-21.jpg' },
@@ -10,9 +14,20 @@ export default function NechavotDemo() {
         { title: "Updated feed", source: 'https://i.ibb.co/tM7C259/new-Feed-4.jpg' },
     ]
 
+    function handleClick() {
+        setShowInstructions(false);
+    }
+
     return (
+        
         <div className="nechavotDemo">
-              {images.map((image, index)=> {
+            {showInstructions && <div className="ins-box" > 
+                <div className='text'> <p> Click on an image to open it </p>
+                <p> Don't forget to click again to close it before opening the next one </p>
+                </div>
+                <button onClick = {handleClick}> Ok </button> 
+            </div>}
+            {images.map((image, index)=> {
                 return <Card key={index} className="card" title={image.title} src={image.source} />
             })}
         </div>
